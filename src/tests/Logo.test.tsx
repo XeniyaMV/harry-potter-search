a2e4b6e101f558fd68a0d9b4acfd950b77dc7986
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-16 23:14:42 -0800

    feat: add tests to Logo component

--- src/tests/Logo.test.tsx
+++ src/tests/Logo.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Logo from '../UI/logo/Logo';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe('Logo', () => {
   it('renders Logo component', () => {
@@ -23,5 +24,49 @@ describe('Logo', () => {
     );
     const logoElement = screen.getByTestId('logo-component');
     expect(logoElement).toHaveClass('logo custom-class');
+  });
+
+  it('renders the Logo component with provided props', () => {
+    const logoProps = {
+      link: '/',
+      title: 'Home',
+      iconUrl: 'path-to-icon.png',
+    };
+
+    render(
+      <MemoryRouter>
+        <Logo {...logoProps} />
+      </MemoryRouter>
+    );
+
+    const titleElement = screen.getByText('Home');
+    const iconElement = screen.getByAltText('logo icon');
+
+    expect(titleElement).toBeInTheDocument();
+    expect(iconElement).toBeInTheDocument();
+
+    expect(titleElement).toHaveTextContent('Home');
+    expect(iconElement).toHaveAttribute('src', 'path-to-icon.png');
+    expect(iconElement).toHaveAttribute('alt', 'logo icon');
+  });
+
+  it('navigates to the correct link when clicked', () => {
+    const history = createMemoryHistory();
+
+    const logoProps = {
+      title: 'My Logo',
+      link: '/home',
+    };
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <Logo {...logoProps} />
+      </Router>
+    );
+
+    const logoElement = screen.getByTestId('logo-component');
+    fireEvent.click(logoElement);
+
+    expect(history.location.pathname).toBe('/home');
   });
 });
